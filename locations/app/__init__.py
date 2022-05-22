6a54a03d94a69352169d0b1f0d6c4e4abbbd9bad
akolzin-lineate <akolzin_lineate@lineate.com>
2022-05-22 13:15:34 +0300

    restructure project

--- locations/app/__init__.py
+++ locations/app/__init__.py
@@ -8,7 +8,8 @@ app = Flask(__name__, instance_relative_config=True)
 user_name = os.environ.get('USER_NAME')
 user_password = os.environ.get('USER_PASSWORD')
 base_name = os.environ.get('BASE_NAME')
-app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{user_name}:{user_password}@localhost:5433/{base_name}'
+posrtgres_port = os.environ.get('PORT')
+app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{user_name}:{user_password}@localhost:{posrtgres_port}/{base_name}'
 db = SQLAlchemy(app)
 
 
